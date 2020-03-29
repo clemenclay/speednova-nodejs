d23572d80a3e92d77bb3780f530dfafb2cc1d65b
clemenclay <clemenclay@gmail.com>
2020-03-28 22:00:04 -0300

    check

---
+++ .factor/loader-settings.ts
@@ -0,0 +1,22 @@
+import { default as dashboard } from "C:/laragon/www/speednova-angular/node_modules/@factor/dashboard/factor-settings" // 100
+import { default as templates } from "C:/laragon/www/speednova-angular/node_modules/@factor/templates/factor-settings" // 100
+import { default as ui } from "C:/laragon/www/speednova-angular/node_modules/@factor/ui/factor-settings" // 100
+import { default as contactForm } from "C:/laragon/www/speednova-angular/node_modules/@factor/plugin-contact-form/factor-settings" // 100
+import { default as blog } from "C:/laragon/www/speednova-angular/node_modules/@factor/plugin-blog/factor-settings" // 100
+import { default as alpha } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/dashboard/factor-settings" // 150
+import { default as alpha_1 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/plugin-contact-form/factor-settings" // 150
+import { default as alpha_2 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/ui/factor-settings" // 150
+import { default as alpha_3 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/src/factor-settings" // 150
+import { default as cwd } from "C:/laragon/www/speednova-angular/node_modules/@factor/dashboard/factor-settings" // 1000
+import { default as cwd_1 } from "C:/laragon/www/speednova-angular/node_modules/@factor/plugin-blog/factor-settings" // 1000
+import { default as cwd_2 } from "C:/laragon/www/speednova-angular/node_modules/@factor/plugin-contact-form/factor-settings" // 1000
+import { default as cwd_3 } from "C:/laragon/www/speednova-angular/node_modules/@factor/templates/factor-settings" // 1000
+import { default as cwd_4 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/dashboard/factor-settings" // 1000
+import { default as cwd_5 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/plugin-contact-form/factor-settings" // 1000
+import { default as cwd_6 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/node_modules/@factor/ui/factor-settings" // 1000
+import { default as cwd_7 } from "C:/laragon/www/speednova-angular/node_modules/@factor/theme-alpha/src/factor-settings" // 1000
+import { default as cwd_8 } from "C:/laragon/www/speednova-angular/node_modules/@factor/ui/factor-settings" // 1000
+import { default as cwd_9 } from "C:/laragon/www/speednova-angular/src/factor-settings" // 1000
+
+
+export default [ dashboard, templates, ui, contactForm, blog, alpha, alpha_1, alpha_2, alpha_3, cwd, cwd_1, cwd_2, cwd_3, cwd_4, cwd_5, cwd_6, cwd_7, cwd_8, cwd_9 ]
